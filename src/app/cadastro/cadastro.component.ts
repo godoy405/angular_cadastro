46c465883b86cd967df1711f07bd54d46a1381ad
Gilberto <gilbertonerigodoy@gmail.com>
2025-12-29 22:27:28 -0300

    Configurando do select municipio com erro de carregamento

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -5,24 +5,32 @@ import { MatCardModule} from '@angular/material/card';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatSelectModule, MatSelectChange } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Cliente} from './cliente';
 import { ClienteService } from '../cliente.service';
 import { NgxMaskDirective, provideNgxMask} from 'ngx-mask'
+import { BrasilapiService } from '../brasilapi.service';
+import { Estado, Municipio } from '../brasilapi.models';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-cadastro',
   imports: [
+    CommonModule,
+    FormsModule,
     FlexLayoutModule,
     MatCardModule,
-    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    NgxMaskDirective
+    MatSelectModule,
+    NgxMaskDirective,
   ], providers: [
     provideNgxMask()
   ],
@@ -34,30 +42,55 @@ export class CadastroComponent implements OnInit {
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
   snack: MatSnackBar = inject(MatSnackBar);
+  estados: Estado[] = [];
+  municipios: Municipio[] = [];
 
   constructor(
     private service: ClienteService,
+    private BrasilapiService: BrasilapiService,
     private route: ActivatedRoute,
     private router: Router
-  ){
-
-  }
+  ){}
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe( (query: any) => {
-      const params = query['params']
-      const id = params['id'];
-      if(id) {
-        let clienteEncontrado = this.service.buscarClientePorId(id);
-        if(clienteEncontrado){
-          this.atualizando = true;
-          this.cliente = clienteEncontrado;
-        }else{
-          this.cliente = Cliente.newCliente();
-        }
+  this.route.queryParamMap.subscribe((query: any) => {
+    const params = query['params'];
+    const id = params['id'];
 
+    if (id) {
+      let clienteEncontrado = this.service.buscarClientePorId(id);
+
+      if (clienteEncontrado) {
+        this.atualizando = true;
+        this.cliente = clienteEncontrado;
+
+        if (this.cliente.uf) {
+          const event = { value: this.cliente.uf };
+          this.carregarMunicipios(event as MatSelectChange);
+        }
+      } else {
+        this.cliente = Cliente.newCliente();
       }
-    });
+    }
+  });
+
+  this.carregarUFs();
+}
+
+  carregarUFs() {
+    //observable  subscriber
+    this.BrasilapiService.listarUFs().subscribe({
+      next: listaEstados => this.estados = listaEstados,
+      error: erro => console.error("erro ao listar estados", erro)
+    })
+  }
+
+  carregarMunicipios(event: MatSelectChange) { // 👈 CORRIGIDO: MatSelectChange
+    const ufSelecionada = event.value;
+    this.BrasilapiService.listarMunicipios(ufSelecionada).subscribe({
+      next: listaMunicipios => this.municipios = listaMunicipios,
+      error: erro => console.error("erro ao listar municipios", erro)
+    })
   }
 
   salvar() {
--- src/app/cadastro/cliente.ts
+++ src/app/cadastro/cliente.ts
@@ -7,6 +7,8 @@ export class Cliente {
     dataNascimento?: string;
     email?: string;
     deletando?: boolean;
+    uf?: string;
+    municipio?: string;
 
     static newCliente() {
         const cliente = new Cliente();
